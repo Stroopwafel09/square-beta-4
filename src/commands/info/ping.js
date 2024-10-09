4b006b07980cf8a7fc4d708cd6530e5331117ddc
Stroopwafel09 <107180056+Stroopwafel09@users.noreply.github.com>
2024-10-09 15:53:35 +0200

    Update ping.js

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -1,22 +1,23 @@
-const {EmbedBuilder, PermissionsBitField} = require('discord.js');
-const {SlashCommandBuilder} = require('@discordjs/builders');
+const { EmbedBuilder, PermissionsBitField } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 exports.commandBase = {
-	prefixData: {
-		name: 'ping',
-		aliases: ['pong'],
-	},
-	slashData: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Pong!'),
-	// If you want to improve the command, check the guide: https://discordjs.guide/slash-commands/advanced-creation.html
-	cooldown: 5000, // 1 second = 1000 ms / set to 0 if you don't want a cooldown.
-	ownerOnly: false, // Set to true if you want the command to be usable only by the developer.
-	async prefixRun(client, message, args) {
-		message.reply('Pong 🏓');
-	},
-	async slashRun(client, interaction) {
-		interaction.reply('Pong 🏓');
-	},
+    prefixData: {
+        name: 'ping',
+        aliases: ['pong'],
+    },
+    slashData: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Pong!'),
+    cooldown: 5000, // 5 seconds cooldown
+    ownerOnly: false, // Not restricted to the owner
+    async prefixRun(client, message, args) {
+        message.reply('Pong 🏓');
+    },
+    async slashRun(client, interaction) {
+        interaction.reply('Pong 🏓');
+    },
 };
-module.exports = ping;
+
+// Ensure you export the commandBase correctly
+module.exports = exports.commandBase;
